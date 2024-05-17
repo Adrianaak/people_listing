0ba8c838d0e3850cc36c89650edc140f9672abce
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-05-17 14:42:24 -0500

    updated files

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,46 @@
 
 window.onload = () => {
 
-    
+    thePeopleListing ();
 
+}
+
+function thePeopleListing() {
+
+    //I need to get a hold of the element
+    let theTableBody = document.querySelector("#thePeopleListingTblBody");
+
+    //need to work with all the lottery tickets
+    people.forEach((person) => {
+
+        //call a function to build the indiviual table rows and data   
+        buildTableRow(theTableBody, person);
+
+
+    });
+
+    //this function is responsible for building and adding a table row to the table body
+    function buildTableRow(tableBody, data) {
+
+        let newRow = tableBody.insertRow(-1);
+        
+        let cell1 = newRow.insertCell();
+        cell1.innerHTML = data.id;
+
+        let cell2 = newRow.insertCell();
+        cell2.innerHTML = data.firstName;
+
+        let cell3 = newRow.insertCell();
+        cell3.innerHTML = data.lastName;
+
+        let cell4 = newRow.insertCell();
+        cell4.innerHTML = data.email;
+
+        let cell5 = newRow.insertCell();
+        cell5.innerHTML = data.ipAddress;
+
+
+
+
+    }
 }
